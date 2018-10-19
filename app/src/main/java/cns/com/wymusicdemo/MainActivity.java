61239c6993768f5d8e2df41d745499d7e3b15e8b
luguowei <358488581@qq.com>
2018-10-19 15:13:43 +0800

    1、test

--- app/src/main/java/cns/com/wymusicdemo/MainActivity.java
+++ app/src/main/java/cns/com/wymusicdemo/MainActivity.java
@@ -1,13 +1,16 @@
 package cns.com.wymusicdemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String title;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
